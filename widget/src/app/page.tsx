58490beb03b0f4ad01b506326be2a7709e4428b5
sudhanshu tiwary <sudhanshukumar321456@gmail.com>
2025-06-03 19:26:29 +0530

    v-1.7

--- widget/src/app/page.tsx
+++ widget/src/app/page.tsx
@@ -41,7 +41,7 @@ export default function Home() {
   }, []);
   
   return (
-    <main className="min-h-screen bg-gradient-to-b from-blue-50 to-white">
+    <main className="min-h-screen ">
       {/* The widget itself */}
       <FloatingWidget />
     </main>
