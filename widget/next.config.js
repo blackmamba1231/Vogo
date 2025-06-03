f060436f4763936847dfc45612acc91203e762f5
sudhanshu tiwary <sudhanshukumar321456@gmail.com>
2025-06-03 15:24:05 +0530

    v-1.3

---
+++ widget/next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    // Disable ESLint during production builds
+    ignoreDuringBuilds: true,
+  },
+  // Add any other Next.js config options here
+};
+
+module.exports = nextConfig;
